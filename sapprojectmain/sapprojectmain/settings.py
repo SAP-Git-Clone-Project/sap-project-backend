be99259b3874a24b5c9f308c43faa2fde57d4d5f
JoeTheBest <jewelshahi10@gmail.com>
2026-03-29 22:58:50 +0300

    Add STATIC_ROOT for Render deployment

--- sapprojectmain/sapprojectmain/settings.py
+++ sapprojectmain/sapprojectmain/settings.py
@@ -53,13 +53,14 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'audit_log.middleware.AuditIPMiddleware',
+    "audit_log.middleware.AuditIPMiddleware",
 ]
 
 # CORS origin
@@ -173,3 +174,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+# Static root for Render
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# WhiteNoise is configured to handle these files
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
