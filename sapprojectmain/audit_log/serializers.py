7504c9e14626f67b3a6e2c1d3ce380326e117a31
JoeTheBest <jewelshahi10@gmail.com>
2026-04-14 03:27:51 +0300

    Changes in the audit log serializer

--- sapprojectmain/audit_log/serializers.py
+++ sapprojectmain/audit_log/serializers.py
@@ -1,17 +1,31 @@
 from rest_framework import serializers  # <--- Add this line!
 from .models import AuditLogModel
 
+
 class AuditLogSerializer(serializers.ModelSerializer):
     username = serializers.ReadOnlyField(source="user.username")
+    first_name = serializers.ReadOnlyField(source="user.first_name")
+    last_name = serializers.ReadOnlyField(source="user.last_name")
+    email = serializers.ReadOnlyField(source="user.email")
     document_title = serializers.ReadOnlyField(source="document.title")
     created_by_avatar_url = serializers.SerializerMethodField()
 
     class Meta:
         model = AuditLogModel
         fields = [
-            'id', 'user', 'username', 'action_type', 'document', 
-            'document_title', 'ip_address', 'timestamp', 
-            'description', 'created_by_avatar_url'
+            "id",
+            "user",
+            "email",
+            "username",
+            "action_type",
+            "document",
+            "first_name",
+            "last_name",
+            "document_title",
+            "ip_address",
+            "timestamp",
+            "description",
+            "created_by_avatar_url",
         ]
 
     def get_created_by_avatar_url(self, obj):
